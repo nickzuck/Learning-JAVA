236da052be0293d35870f97b7e41e4498c50b5c4
nickzuck <nikhil1945.singh@gmail.com>
2015-09-09 15:13:46 +0530

    Runs without a throws near the function

---
+++ 9thSep2015.java
@@ -0,0 +1,22 @@
+class Except
+{
+    public static void main (String [] args) {
+        try {
+            System.out.println("We are calling the function\n") ; 
+            callhim() ;
+        }
+        catch (Exception a){
+            System.out.println ("You got this exception  "+ a) ; 
+        }
+    }
+    
+    static void callhim()
+    {
+        int a = 24 ;  
+        int b= 0; 
+        int ans = a/b ; 
+        System.out.println("The answer is " + ans ) ; 
+    }
+}
+
+
