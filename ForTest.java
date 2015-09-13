37158f3750ce98fab258be042671f0ebf305291d
nickzuck <nikhil1945.singh@gmail.com>
2015-09-13 17:43:30 +0530

    We can use for statement like this to access the array

---
+++ ForTest.java
@@ -0,0 +1,9 @@
+class ForTest
+{
+    public static void main (String []  args){
+        int v [] = {1 ,3 , 4 ,5 } ;  
+        for (int x : v) {
+            System.out.println(x) ; 
+        } 
+    }
+}
