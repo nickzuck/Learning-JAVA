54e015872296e031d79cfa41e77be70f313e1c23
nickzuck <nikhil1945.singh@gmail.com>
2015-09-13 17:36:06 +0530

    Added the remaining fiiles

---
+++ nickzuck/StrandStrBuf.java
@@ -0,0 +1,19 @@
+import java.util.* ; 
+class StrandStrBuf
+{
+    Scanner input = new Scanner (System.in) ; 
+    int i; 
+    public static void main (String [] args) 
+    {
+        System.out.println("1.) String\n2.) StringBuffer\nEnter your choice:\t");           
+        i = input.nextInt() ;  
+        switch(i){
+            case (1) :
+                System.out.println("Welcome to String arena\nEnter a string") ;
+                 
+                
+                        
+        } 
+    }
+    
+}
