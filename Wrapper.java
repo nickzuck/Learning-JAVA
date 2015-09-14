5a30d920a0b7a1a824d9f953af42deccad292468
nickzuck <nikhil1945.singh@gmail.com>
2015-09-14 17:56:15 +0530

    First stage of working with wrappers

---
+++ Wrapper.java
@@ -0,0 +1,31 @@
+class Wrapper 
+{
+
+        int i = 10 ; 
+        char c = 'a'; 
+        boolean b = true ; 
+        double d = 20.32 ; 
+        float f = 12.3f ; 
+        long l = 132 ; 
+        short s = 3 ;
+        byte byt = 3; 
+        
+    public static void main (String[] args){
+        Wrapper object = new Wrapper() ; 
+        object.print();
+    }
+    void print(){
+        System.out.println("Integer = " + i ) ; 
+        System.out.println("Character = " + c );
+        System.out.println("Boolean = " + b);
+        System.out.println("Double = " + d );
+        System.out.println("Float = " + f); 
+        System.out.println("Long = " + l);
+        System.out.println("Short = "+ s);        
+        System.out.println("Byte = "+ byt);
+        Character ch = new Character(c) ; 
+        System.out.println ("\nWrapper Objects\n"); 
+        System.out.println (ch) ;
+        System.out.println(ch.charValue()) ;
+    } 
+}
