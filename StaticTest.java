ae4e2e27c1e8c7d09cb7fcc1b4476111412d5aad
nickzuck <nikhil1945.singh@gmail.com>
2015-09-14 17:08:45 +0530

    you can compile successfully the program without main but can't run it ...Rest of the details in the program print statements

--- StaticTest.java
+++ StaticTest.java
@@ -2,5 +2,10 @@ class StaticTest
 {
     static {
         System.out.println("we are printing this from static without main");
+        System.out.println("Actually you can't do anything without main method int  the program ...It compiles successfully but doesn't run");
+        System.out.println("Static block is accessed first..Although main is also static so we must take care of the order in which the static blocks appear") ; 
+    }
+    public static void main (String [] args) {
+        
     }
 }
