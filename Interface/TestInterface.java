cf1ccb9e9c6ce66f2c66bf16bd65ff3ca5b294e8
nickzuck <nikhil1945.singh@gmail.com>
2015-09-17 15:16:03 +0530

    Interface reference variables only have knowledge of the methods declared by its interface declaration

--- Interface/TestInterface.java
+++ Interface/TestInterface.java
@@ -1,8 +1,9 @@
 class TestInterface
 {
     public static void main (String [] args){
-        Callback c = new Client () ; 
+        Callback c = new Client () ; // Look at it again ...Found out ?? ;) 
         c.callback (123 ) ; 
+        //c.noIfaceMeth() ;  Will give an error
         Client var = new Client () ; 
         var.noIfaceMeth() ;
     }
