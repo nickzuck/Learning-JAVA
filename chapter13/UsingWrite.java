0d847f6d75e61fe765dbd11d6a7fff7d513fa2ad
nickzuck <nikhil1945.singh@gmail.com>
2015-11-07 22:34:04 +0530

    Write can't be used for priting string

---
+++ chapter13/UsingWrite.java
@@ -0,0 +1,9 @@
+class WriteDemo{
+    public static void main(String [] args){
+        String b[] = new String [100] ; 
+        
+        b = "A1";
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
