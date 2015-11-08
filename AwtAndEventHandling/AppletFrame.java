9fa0937dccc112bfda2d5674f06b00ebf5cf49d7
nickzuck <nikhil1945.singh@gmail.com>
2015-11-08 20:10:26 +0530

    Created a frame window inside and appelt

---
+++ AwtAndEventHandling/AppletFrame.java
@@ -0,0 +1,58 @@
+/*Applet and Frame in a window*/
+
+import java.awt.* ; 
+import java.awt.event.*; 
+import java.applet.* ; 
+/*
+    <applet code = "AppletFrame" width = 300 height = 50>
+    </applet>
+*/
+
+//Create a subclass of frame
+class SampleFrame extends Frame{
+    SampleFrame(String title){
+        super(title);
+        
+        //create an object to handle window events
+        MyWindowAdapter adapter = new MyWindowAdapter(this);
+    
+        //register it to receive those events 
+        addWindowListener(adapter);
+    }
+
+    public void paint(Graphics g){
+        g.drawString("This is in frame window" , 10 , 40);
+    }
+}
+
+class MyWindowAdapter extends WindowAdapter{
+    SampleFrame sampleFrame ; 
+    public MyWindowAdapter(SampleFrame sampleframe){
+        this.sampleFrame = sampleFrame ; 
+    }
+
+    public void windowClosing(WindowEvent we){
+        sampleFrame.setVisible(false);
+    }
+}
+
+public class AppletFrame extends Applet{
+    Frame f ;
+    public void init(){
+        f = new SampleFrame("A frame window");
+        f.setSize(250 , 250);
+        f.setVisible(true);
+    }
+
+    public void start(){
+        f.setVisible(true);
+    }
+
+    public void stop(){
+        f.setVisible(false);
+    }
+
+    public void paint(Graphics g){
+        g.drawString("This is in applet window" , 10 , 10);
+    }
+}
