266c45ae53e4aa67409ddfdc94a45b517ff878b6
nickzuck <nikhil1945.singh@gmail.com>
2015-11-08 21:12:13 +0530

    Button Demo in applet ..Cleared :)

---
+++ AwtAndControlLayouts/ButtonDemo.java
@@ -0,0 +1,47 @@
+import java.awt.* ; 
+import java.awt.event.* ; 
+import java.applet.* ; 
+
+/*
+    <applet code = "ButtonDemo" width = 200 height = 200>
+    </applet>
+*/
+
+
+public class ButtonDemo extends Applet implements ActionListener{
+    String msg = "";
+    Button yes , no , maybe ; //For making buttons
+    
+    public void init(){
+        yes = new Button("YES");
+        no = new Button("NO");
+        maybe = new Button("MAYBE");
+
+        //Adding buttons to the applet
+        add(yes);
+        add(no);
+        add(maybe);
+
+
+        /*An ActionListener is a callback mechanism. Whenever a control it is added to fires an ActionEvent, the public void actionPerformed(ActionEvent e) method will be invoked.*/
+        yes.addActionListener(this);
+        no.addActionListener(this); 
+        maybe.addActionListener(this) ; 
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        String str = ae.getActionCommand();
+        if(str.equals("YES"))
+            msg = "You pressed yes";
+        else if(str.equals("NO"))
+            msg = "You pressed no";
+        else 
+            msg = "You pressed maybe";
+
+        repaint();
+    }
+
+    public void paint(Graphics g){
+        g.drawString(msg, 6 , 100);
+    }
+}
